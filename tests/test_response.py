5d35930827185943734296ee5874a3204978fdb2
Vladyslav Burzakovskyy <vladislav.burzakovskij@satoshilabs.com>
2018-12-12 15:28:44 +0100

    fix print spacing in tests

--- tests/test_response.py
+++ tests/test_response.py
@@ -2,6 +2,7 @@ import unittest
 from pyvies import Vies
 from .vat_ids import VALID_VAT_IDS, INVALID_VAT_IDS
 
+
 class TestResponse(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestResponse, self).__init__(*args, **kwargs)
@@ -12,12 +13,13 @@ class TestResponse(unittest.TestCase):
         for vat_number in VALID_VAT_IDS:
             print('Testing "%s"' % vat_number)
             response = self.vies_api.request(vat_number)
-            print('%s\n' % response)
+            print('%s' % response)
 
             if response.error != 'TIMEOUT':
                 self.assertEqual(response.is_valid, True)
             else:
                 print('Request timeouted, skipping...')
+            print()
 
     def test_2_invalid(self):
         print('Testing invalid VAT numbers (without specifying the country code)...')
@@ -30,6 +32,7 @@ class TestResponse(unittest.TestCase):
                 self.assertEqual(response.is_valid, False)
             else:
                 print('Request timeouted, skipping...')
+            print()
 
 
 if __name__ == '__main__':
